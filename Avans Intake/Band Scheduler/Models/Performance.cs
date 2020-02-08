c10b1d0a90168233ee804ec1ffcabcc154096e3d
Rico Schouten <rico@schouten.me>
2020-02-08 18:05:51 +0100

    Add data models

---
+++ Avans Intake/Band Scheduler/Models/Performance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Band_Scheduler.Models
+{
+    class Performance
+    {
+        public int Id { get; set; }
+        public Performer Performer { get; set; }
+        public Stage Stage { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
